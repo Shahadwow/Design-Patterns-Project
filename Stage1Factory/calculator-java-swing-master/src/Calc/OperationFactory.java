73e9bdd64912e478d30b77879de9701290f1322a
Shahadwow <2211320@uj.edu.sa>
2025-11-04 02:11:01 +0300

    Add files via upload

---
+++ Stage1Factory/calculator-java-swing-master/src/Calc/OperationFactory.java
@@ -0,0 +1,15 @@
+
+package Calc;
+
+class OperationFactory {
+
+    public static Operation createOperation(String operator) {
+        switch (operator) {
+            case "+": return new AddOperation();
+            case "-": return new SubtractOperation();
+            case "×": return new MultiplyOperation();
+            case "÷": return new DivideOperation();
+            default: throw new IllegalArgumentException("Unknown operation: " + operator);
+        }
+    }
+}
